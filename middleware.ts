37198883232d98a8c6b4244da8be7227f88130af
SAITAMA40 <onepunchmansensei40@gmail.com>
2025-06-06 16:04:51 +0530

    Fix Clerk middleware configuration

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,10 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard", "/my-resume/:resumeId/edit"]);
-
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
-    auth().protect();
-  }
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in", "/sign-up"],
+  ignoredRoutes: ["/api/webhook"]
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
